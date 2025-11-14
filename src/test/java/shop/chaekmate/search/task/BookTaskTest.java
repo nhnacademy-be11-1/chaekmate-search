1f25bad64c78ab7be6704c0c5e88baa826b32ef1
leegyeongsik <67450537+leegyeongsik@users.noreply.github.com>
2025-11-14 20:30:06 +0900

    feat : 같은 북id dml을 여러개 요청했을때 요청온 순서대로 처리  * feat : response 공통화 처리  * feat : 검색 페이지네이션 추가  * feat : book 변경  * feat : rerank추가  * feat : 주석제거  * feat : ci-cd RERANK_API_URL 추가  * feat : image 리스트에서 string으로 수정  * feat : SPRING_PROFILES_ACTIVE: prod 주석  * feat : rerank수정
    * Fix/book (#12)
    
    * feat : 같은 북id dml을 여러개 요청했을때 요청온 순서대로 처리
    
    * feat : response 공통화 처리
    
    * feat : 검색 페이지네이션 추가
    
    * feat : book 변경
    
    * feat : rerank추가
    
    * feat : 주석제거
    
    * feat : ci-cd RERANK_API_URL 추가
    
    * feat : image 리스트에서 string으로 수정
    
    * feat : SPRING_PROFILES_ACTIVE: prod 주석
    
    * feat : rerank수정

--- src/test/java/shop/chaekmate/search/task/BookTaskTest.java
+++ src/test/java/shop/chaekmate/search/task/BookTaskTest.java
@@ -49,8 +49,9 @@ class BookTaskTest {
     private TaskMapping<?> insertEvent;
     private TaskMapping<?> deleteEvent;
     TaskMapping<?> updateEvent;
+
     @BeforeEach()
-    void init(){
+    void init() {
         BookInfoRequest bookInfoRequestInsert = new BookInfoRequest();
         BookInfoRequest bookInfoRequestUpdate = new BookInfoRequest();
         BookDeleteRequest bookDeleteRequestDelete = new BookDeleteRequest();
@@ -79,9 +80,9 @@ class BookTaskTest {
         Book book2 = Book.builder().id(1).bookImages("tset").author("updatetest").categories(List.of()).description("updatetest").embedding(new Float[]{0.1F,}).price(10000).publicationDatetime(LocalDate.now()).tags(List.of()).title("updatetest").build();
 
         bookDeleteRequestDelete.setId(1);
-        this.insertEvent = new TaskMapping<>(EventType.INSERT,bookInfoRequestInsert);
-        this.updateEvent = new TaskMapping<>(EventType.UPDATE,bookInfoRequestUpdate);
-        this.deleteEvent = new TaskMapping<>(EventType.DELETE,bookDeleteRequestDelete);
+        this.insertEvent = new TaskMapping<>(EventType.INSERT, bookInfoRequestInsert);
+        this.updateEvent = new TaskMapping<>(EventType.UPDATE, bookInfoRequestUpdate);
+        this.deleteEvent = new TaskMapping<>(EventType.DELETE, bookDeleteRequestDelete);
         when(bookIndexService.insert(any())).thenReturn(book1);
         when(bookIndexService.update(any())).thenReturn(book2);
         doNothing().when(bookIndexService).delete(any());
@@ -92,8 +93,9 @@ class BookTaskTest {
         saveQueue.clear();
         bookWaitingTask.clear();
     }
+
     @Test
-    void 컨슈머는_데이터를_이벤트큐에넣음(){
+    void 컨슈머는_데이터를_이벤트큐에넣음() {
         consumer.consume((TaskMapping<BaseBookTaskDto>) insertEvent);
         consumer.consume((TaskMapping<BaseBookTaskDto>) updateEvent);
         consumer.consume((TaskMapping<BaseBookTaskDto>) deleteEvent);
@@ -104,8 +106,9 @@ class BookTaskTest {
         await().atMost(3, SECONDS).until(() -> eventQueue.getSize() == 0);
 
     }
+
     @Test
-    void insert_이벤트는_임베딩큐를_거쳐_세이브큐에서_저장(){
+    void insert_이벤트는_임베딩큐를_거쳐_세이브큐에서_저장() {
         consumer.consume((TaskMapping<BaseBookTaskDto>) insertEvent);
         verify(eventQueue, timeout(5000).times(1)).offer(any());
         verify(eventQueue, timeout(5000).atLeast(1)).take();
@@ -113,13 +116,14 @@ class BookTaskTest {
         verify(embeddingQueue, timeout(5000).times(1)).offer(any());
         verify(embeddingQueue, timeout(5000).atLeast(1)).take();
 
-        verify(deleteQueue,times(0)).offer(any());
+        verify(deleteQueue, times(0)).offer(any());
 
-        verify(saveQueue,times(1)).offer(any());
+        verify(saveQueue, times(1)).offer(any());
         verify(saveQueue, timeout(5000).atLeast(1)).poll();
     }
+
     @Test
-    void update_이벤트는_임베딩큐를_거쳐_세이브큐에_저장(){
+    void update_이벤트는_임베딩큐를_거쳐_세이브큐에_저장() {
 
         consumer.consume((TaskMapping<BaseBookTaskDto>) updateEvent);
         verify(eventQueue, timeout(5000).times(1)).offer(any());
@@ -128,14 +132,15 @@ class BookTaskTest {
         verify(embeddingQueue, timeout(5000).times(1)).offer(any());
         verify(embeddingQueue, timeout(5000).atLeast(1)).take();
 
-        verify(deleteQueue,times(0)).offer(any());
+        verify(deleteQueue, times(0)).offer(any());
 
-        verify(saveQueue,times(1)).offer(any());
+        verify(saveQueue, times(1)).offer(any());
         verify(saveQueue, timeout(5000).atLeast(1)).poll();
 
     }
+
     @Test
-    void delete_이벤트는_딜리트큐에서_삭제(){
+    void delete_이벤트는_딜리트큐에서_삭제() {
         consumer.consume((TaskMapping<BaseBookTaskDto>) deleteEvent);
         verify(eventQueue, timeout(5000).times(1)).offer(any());
         verify(eventQueue, timeout(5000).atLeast(1)).take();
@@ -143,13 +148,13 @@ class BookTaskTest {
         verify(deleteQueue, timeout(5000).times(1)).offer(any());
         verify(deleteQueue, timeout(5000).atLeast(1)).take();
 
-        verify(embeddingQueue,times(0)).offer(any());
-        verify(saveQueue,times(0)).offer(any());
+        verify(embeddingQueue, times(0)).offer(any());
+        verify(saveQueue, times(0)).offer(any());
     }
 
 
     @Test
-    void 여러_이벤트가_순서대로_처리되는지검증(){
+    void 여러_이벤트가_순서대로_처리되는지검증() {
         consumer.consume((TaskMapping<BaseBookTaskDto>) insertEvent);
         consumer.consume((TaskMapping<BaseBookTaskDto>) updateEvent);
         consumer.consume((TaskMapping<BaseBookTaskDto>) deleteEvent);
@@ -160,8 +165,6 @@ class BookTaskTest {
         });
 
     }
-
-
 
 
 }
