b9fb007fa0eb4d3924f42fa5760b268b5e8290e2
leegyeongsik <67450537+leegyeongsik@users.noreply.github.com>
2025-11-14 18:30:53 +0900

    Fix/book (#12) (#13)
    * feat : 같은 북id dml을 여러개 요청했을때 요청온 순서대로 처리
    
    * feat : response 공통화 처리
    
    * feat : 검색 페이지네이션 추가
    
    * feat : book 변경
    
    * feat : rerank추가
    
    * feat : 주석제거
    
    * feat : ci-cd RERANK_API_URL 추가
    
    * feat : image 리스트에서 string으로 수정
    
    * feat : SPRING_PROFILES_ACTIVE: prod 주석
    
    * feat : rerank수정

--- src/test/java/shop/chaekmate/search/controller/SearchControllerTest.java
+++ src/test/java/shop/chaekmate/search/controller/SearchControllerTest.java
@@ -1,78 +1,78 @@
-//package shop.chaekmate.search.controller;
-//
-//import org.junit.jupiter.api.Test;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.data.domain.Page;
-//import org.springframework.data.domain.PageImpl;
-//import org.springframework.data.domain.PageRequest;
-//import org.springframework.data.domain.Pageable;
-//import org.springframework.http.MediaType;
-//import org.springframework.test.context.ActiveProfiles;
-//import org.springframework.test.context.bean.override.mockito.MockitoBean;
-//import org.springframework.test.web.servlet.MockMvc;
-//import shop.chaekmate.search.document.Book;
-//import shop.chaekmate.search.dto.SearchResponse;
-//import shop.chaekmate.search.service.SearchService;
-//
-//import java.time.LocalDate;
-//import java.util.List;
-//
-//import static org.mockito.ArgumentMatchers.any;
-//import static org.mockito.ArgumentMatchers.anyString;
-//import static org.mockito.Mockito.when;
-//import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-//import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-//import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-//
-//@SpringBootTest
-//@AutoConfigureMockMvc
-//@ActiveProfiles("test")
-//class SearchControllerTest {
-//    @Autowired
-//    private MockMvc mockMvc;
-//    @MockitoBean
-//    private SearchService searchService;
-//
-//    @Test
-//    void 검색() throws Exception {
-//        Pageable pageable = PageRequest.of(0, 10);
-//
-//        Page<SearchResponse> mockResponses = new PageImpl<>(List.of(
-//                new SearchResponse(Book.builder()
-//                        .id(1L)
-//                        .title("zzz")
-//                        .author("zzz")
-//                        .price(18000)
-//                        .description("zzzzz")
-//                        .reviewCnt(0)
-//                        .categories(List.of("zz", "zzz"))
-//                        .tags(List.of("zzz", "zzz"))
-//                        .publicationDatetime(LocalDate.now())
-//                        .build()),
-//                new SearchResponse(Book.builder()
-//                        .id(2L)
-//                        .title("zzzzz")
-//                        .author("zzz")
-//                        .price(20000)
-//                        .reviewCnt(0)
-//                        .description("zzzzz")
-//                        .categories(List.of("zz", "zzz"))
-//                        .tags(List.of("zzz", "zzz"))
-//                        .publicationDatetime(LocalDate.now())
-//                        .build())
-//        ),pageable,2);
-//
-//        when(searchService.search(anyString(), any(Pageable.class))).thenReturn(mockResponses);
-//
-//        mockMvc.perform(get("/search")
-//                        .param("prompt", "zzz")
-//                        .accept(MediaType.APPLICATION_JSON))
-//                .andExpect(status().isOk())
-//                .andExpect(jsonPath("$.code").value("SUCCESS-200"))
-//                .andExpect(jsonPath("$.data.content[0].title").value("zzz"))
-//                .andExpect(jsonPath("$.data.content[1].title").value("zzzzz"))
-//                .andExpect(jsonPath("$.data.totalElements").value(2));
-//    }
-//}
+package shop.chaekmate.search.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+import shop.chaekmate.search.document.Book;
+import shop.chaekmate.search.dto.SearchResponse;
+import shop.chaekmate.search.service.SearchService;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@ActiveProfiles("test")
+class SearchControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+    @MockitoBean
+    private SearchService searchService;
+
+    @Test
+    void 검색() throws Exception {
+        Pageable pageable = PageRequest.of(0, 10);
+
+        Page<SearchResponse> mockResponses = new PageImpl<>(List.of(
+                new SearchResponse(Book.builder()
+                        .id(1L)
+                        .title("zzz")
+                        .author("zzz")
+                        .price(18000)
+                        .description("zzzzz")
+                        .reviewCnt(0)
+                        .categories(List.of("zz", "zzz"))
+                        .tags(List.of("zzz", "zzz"))
+                        .publicationDatetime(LocalDate.now())
+                        .build()),
+                new SearchResponse(Book.builder()
+                        .id(2L)
+                        .title("zzzzz")
+                        .author("zzz")
+                        .price(20000)
+                        .reviewCnt(0)
+                        .description("zzzzz")
+                        .categories(List.of("zz", "zzz"))
+                        .tags(List.of("zzz", "zzz"))
+                        .publicationDatetime(LocalDate.now())
+                        .build())
+        ),pageable,2);
+
+        when(searchService.search(anyString(), any(Pageable.class))).thenReturn(mockResponses);
+
+        mockMvc.perform(get("/search")
+                        .param("prompt", "zzz")
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.code").value("SUCCESS-200"))
+                .andExpect(jsonPath("$.data.content[0].title").value("zzz"))
+                .andExpect(jsonPath("$.data.content[1].title").value("zzzzz"))
+                .andExpect(jsonPath("$.data.totalElements").value(2));
+    }
+}
