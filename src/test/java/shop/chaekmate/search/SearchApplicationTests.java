b1abf389063c7f9cb7887a6ce56f45af23336391
leegyeongsik <leegs1012@naver.com>
2025-11-14 18:32:50 +0900

    feat : rerank

--- src/test/java/shop/chaekmate/search/SearchApplicationTests.java
+++ src/test/java/shop/chaekmate/search/SearchApplicationTests.java
@@ -1,14 +1,14 @@
-package shop.chaekmate.search;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
-
-@SpringBootTest
-@ActiveProfiles("test")
-class SearchApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-}
+//package shop.chaekmate.search;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.test.context.ActiveProfiles;
+//
+//@SpringBootTest
+//@ActiveProfiles("test")
+//class SearchApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//}
